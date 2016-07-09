5cfdfdb681439bebe530db035fe445a29bb3db99
William Immendorf <will.immendorf@gmail.com>
2016-07-08 21:29:42 -0500

    Remove uneeded files from procedual version

--- rpn.hpp
+++
@@ -1,8 +0,0 @@
-// Function definitions for Reverse Polish Notation parser
-// William Immendorf - 2016
-
-#include <queue>
-#include <string>
-
-std::queue<char> convert_to_rpn(std::string expression);
-int evaluate(std::queue<char> expression);
