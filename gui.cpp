ac7bfc39e4770250ff404ba7b34cf3a98e94aa41
William Immendorf <will.immendorf@gmail.com>
2016-10-07 23:38:45 -0500

    Begin GUI work

--- gui.cpp
+++ gui.cpp
@@ -3,20 +3,36 @@
 
 #include <iostream>
 #include <string>
+
 #include <SDL.h>
+#include <SDL_opengl.h>
+#include <GL/glu.h>
 
-#include "equation.hpp"
+#include "varequation.hpp"
 
-const int int_screen_width = 800
-const int int_screen_height = 600
+const int screen_width = 800;
+const int screen_height = 600;
+
+const int x_scale = 10;
+const int y_scale = 10;
 
 SDL_Window* window = nullptr;
-SDL_Renderer* renderer = nullptr;
+SDL_GLContext gl_context;
 
+// Initialize SDL
 bool init();
 
-void close();
+// Initalize GL subsystem
+bool initGL();
 
+// Render grid to screen
+void renderGrid();
+
+// Render line, given a VariableEquation
+void renderLine(EquParser::VariableEquation equation);
+
+// Close SDL and free memory
+void close();
 
 bool init()
 {
@@ -26,32 +42,44 @@ bool init()
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		// Oops, something happened with SDL
-		std::cout << "Cannot initalize SDL. Reason: " << SDL_GetError() << std::endl;
+		std::cerr << "Cannot initalize SDL. Reason: " << SDL_GetError() << std::endl;
 		success = false;
 	}
 	else
 	{
+		// Use OpenGL 2.1
+		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
+		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
+
 		// We've made it this far, so let's create the main window
-		window = SDL_CreateWindow("Graphic Plotter", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_RESIZABLE);
+		window = SDL_CreateWindow("Graphic Plotter", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 		if (window == nullptr)
 		{
 			// No window, so log the reason why and get out of here
-			std::cout << "Cannot create window. Reason: " << SDL_GetError() << std::endl;
+			std::cerr << "Cannot create window. Reason: " << SDL_GetError() << std::endl;
 			success = false;
 		}
 		else
 		{
-			// Now create the window's renderer
-			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-			if (renderer == nullptr)
+			// Now create the OpenGL context
+			gl_context = SDL_GL_CreateContext(window);
+			if (gl_context == nullptr)
 			{
-				std::cout << "Cannot create renderer. Reason: " << SDL_GetError() << std::endl;
+				std::cerr << "Unable to initalize OpenGL context! Reason: " << SDL_GetError() << std::endl;
 				success = false;
 			}
 			else
 			{
-				// Set the rendering color to opaque plain white.
-				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				// Enable Vsync
+				if (SDL_GL_SetSwapInterval(1) < 0)
+				{
+					std::clog << "Warning: Vsync not enabled. Reason: " << SDL_GetError() << std::endl;
+				}
+
+				// Now actually initialize GL
+				if (!initGL())
+				{
+					success = false;
 				}
 			}
 		}
@@ -59,3 +87,87 @@ bool init()
 	return success;
 }
 
+bool initGL()
+{
+	// Set up orthographic projection
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, screen_width, 0.0, screen_height, 1.0, -1.0);
+
+	// Set up modelview matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Set clear color to pure white
+	glClearColor(1.f, 1.f, 1.f, 1.f);
+
+	// Check for any errors
+	GLenum error = glGetError();
+	if (error != GL_NO_ERROR)
+	{
+		std::cerr << "Cannot initalize OpenGL. Reason: " << gluErrorString(error);
+		return false;
+	}
+
+	return true;
+}
+
+void renderGrid()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Reset and select modelview matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Move created objects to center of screen
+	glTranslatef(screen_width / 2.f, screen_height / 2.f, 0.f);
+	
+	// Set drawing color
+	glColor3f(0.f, 0.f, 0.f);
+
+	// Render origin lines
+	glLineWidth(4);
+	glBegin(GL_LINES);
+		glVertex2f(0, -(screen_height / 2.f));
+		glVertex2f(0, (screen_height / 2.f));
+		glVertex2f(-(screen_width / 2.f), 0);
+		glVertex2f((screen_width / 2.f), 0);
+	glEnd();
+}
+
+void close()
+{
+	SDL_DestroyWindow(window);
+	window = nullptr;
+
+	SDL_Quit();
+}
+
+int main(int argc, char* argv[])
+{
+	if (init())
+	{
+		bool quit = false;
+
+		SDL_Event e;
+
+		while (!quit)
+		{
+			while (SDL_PollEvent(&e) != 0)
+			{
+				if (e.type == SDL_QUIT)
+					quit = true;
+				else if (e.type == SDL_KEYDOWN)
+				{
+					if (e.key.keysym.sym == SDLK_ESCAPE)
+						quit = true;
+				}
+			}
+			renderGrid();
+			SDL_GL_SwapWindow(window);
+		}
+	}
+	
+	close();
+}
